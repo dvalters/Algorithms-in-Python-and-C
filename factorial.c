34e5a76232f5ad3c6bc3572da4918aab360d0e44
decvalts <declan.valters@manchester.ac.uk>
2017-03-31 23:38:54 +0100

    Added Example from Chap.2 - factorial

---
+++ factorial.c
@@ -0,0 +1,21 @@
+#include "factorial.h"
+
+int factorial(int n) {
+  if (n < 0)
+  {
+    return 0;
+  }
+  else if (n == 0)
+  {
+    return 1;
+  }
+  else if (n == 1)
+  {
+    return 1;
+  }
+  else 
+  {
+    return n * factorial(n-1);
+  }
+}
+
---
+++ factorial.h
@@ -0,0 +1,2 @@
+int factorial(int n);
+
