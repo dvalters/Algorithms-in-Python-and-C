d2dc1e2663a54479ec862e31959f9ea09f995957
decvalts <declan.valters@manchester.ac.uk>
2018-02-23 23:34:01 +0000

    Recursion python examples

---
+++ python/viz_factorial.py
@@ -0,0 +1,17 @@
+from rcviz import CallGraph, viz
+
+cg = CallGraph(filename="factorial.png")
+
+@viz(cg)
+def factorial(n):
+  """Takes a number n, and calculates it's factorial using recursion"""
+  if n < 0:
+    return 0
+  elif n == 0 or n == 1:
+    return 1
+  else:
+    return n * factorial(n - 1)
+
+factorial(4)
+cg.render()
+
